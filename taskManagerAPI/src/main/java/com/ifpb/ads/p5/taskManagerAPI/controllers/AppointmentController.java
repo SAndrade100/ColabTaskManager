7e9ed2bbf9cff754ff26dcb174844194d376896b
Samuel Andrade de Araújo <andradedearaujo100@gmail.com>
2024-11-12 21:20:28 -0300

    Adding routes for appointment.

--- taskManagerAPI/src/main/java/com/ifpb/ads/p5/taskManagerAPI/controllers/AppointmentController.java
+++ taskManagerAPI/src/main/java/com/ifpb/ads/p5/taskManagerAPI/controllers/AppointmentController.java
@@ -1,12 +1,18 @@
 package com.ifpb.ads.p5.taskManagerAPI.controllers;
 
 import com.ifpb.ads.p5.taskManagerAPI.models.Appointment;
+import com.ifpb.ads.p5.taskManagerAPI.models.Task;
+import com.ifpb.ads.p5.taskManagerAPI.models.User;
 import com.ifpb.ads.p5.taskManagerAPI.services.AppointmentService;
+import com.ifpb.ads.p5.taskManagerAPI.services.TaskService;
+import com.ifpb.ads.p5.taskManagerAPI.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/appointments")
@@ -14,6 +20,12 @@ public class AppointmentController {
 
     @Autowired
     private AppointmentService appointmentService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TaskService taskService;
 
     @GetMapping
     public List<Appointment> getAllAppointments() {
@@ -52,5 +64,34 @@ public class AppointmentController {
     public ResponseEntity<Void> deleteAppointment(@PathVariable long id) {
         appointmentService.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/schedule")
+    public ResponseEntity<Appointment> scheduleAppointment(@RequestParam long userId, @RequestParam long taskId) {
+        User user = userService.findById(userId);
+        Task task = taskService.findById(taskId);
+
+        if(user == null || task == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Appointment appointment = new Appointment();
+        appointment.setUser(user);
+        appointment.setTask(task);
+        appointment.setDataHora(new Date());
+
+        Appointment savedAppointment = appointmentService.save(appointment);
+        return ResponseEntity.ok(savedAppointment);
+    }
+
+    @DeleteMapping("/unschedule")
+    public ResponseEntity<Void> unscheduleAppointment(@RequestParam long userId, @RequestParam long taskId) {
+        Optional<Appointment> appointment = appointmentService.findByUserIdAndTaskId(userId, taskId);
+        if (appointment.isPresent()) {
+            appointmentService.delete(appointment.get().getId());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
